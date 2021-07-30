f743bad5a7773f3202819a9289a6d85e15c36178
SubhamB13 <84260788+SubhamB13@users.noreply.github.com>
2021-07-30 23:04:43 +0530

    Update player.js

--- player.js
+++ player.js
@@ -6,7 +6,7 @@ class player{
         this.body = Bodies.rectangle(x,y,width,height,options)
         this.width = width
         this.height = height
-        this.image = loadImage("./assets/player.png")
+        this.image = loadImage("player.png")
 
         World.add(world,this.body)
     }
